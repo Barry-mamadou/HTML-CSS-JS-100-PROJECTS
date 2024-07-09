74c5fb7826895784e811e9cac002e42fa3374356
Barry <barrymamadou58@gmail.com>
2024-07-10 07:29:13 +1100

    Custom Cursor

---
+++ 17. Creating A Custom Cursor/app.js
@@ -0,0 +1,9 @@
+let cursor = document.querySelector(".cursor");
+
+const moveCursor = (pageX, pageY) => {
+  cursor.style.left = pageX + "px";
+  cursor.style.top = pageY + "px";
+};
+document.addEventListener("mousemove", (event) => {
+  moveCursor(event.pageX, event.pageY);
+});
