8a0ce36dd93a4a0f2628a9a6f6060fb8d4161ee9
Barry <barrymamadou58@gmail.com>
2024-07-12 12:31:22 +1100

    Random Emojis

---
+++ 21. Random Emoji/app.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector(".emoji");
+
+const emojis = [
+  "👍",
+  "🔥",
+  "🥂",
+  "😉",
+  "😊",
+  "🤣",
+  "❤",
+  "😍",
+  "😒",
+  "👌",
+  "😘",
+  "💕",
+  "😁",
+  "🙌",
+  "🤦‍♀️",
+  "🤦‍♂️",
+  "🤷‍♀️",
+  "🤷‍♂️",
+  "✌",
+  "🤞",
+  "😎",
+];
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
